6d1cc7572b19391c8406f6a96a1e49c4f058fc70
Derek Berggren <HollowedEmpire@gmail.com>
2021-06-14 18:14:19 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Derek Berggren on 6/14/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
